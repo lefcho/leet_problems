c98052212ca9146db6fe94987851ce08d0e7f65f
ivand <ivan.d.kostov@gmail.com>
2025-09-07 22:21:01 +0300

    added pr 66 second solution

--- 66_plus_one.py
+++ 66_plus_one.py
@@ -11,3 +11,18 @@ class Solution:
             mult *= 10
         
         return [int(d) for d in str(result)]
+    
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+
+        for i in range(len(digits) - 1, -1, -1):
+
+            if digits[i] + 1 != 10:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+
+            if i == 0:
+                return [1] + digits
