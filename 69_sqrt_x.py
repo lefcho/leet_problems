361a0dac51e8d808abf66be603d5b72ad198a8de
ivand <ivan.d.kostov@gmail.com>
2025-09-12 16:16:16 +0300

    added pr 69

---
+++ 69_sqrt_x.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        i = 1
+        while True:
+            if i * i == x:
+                return i
+            elif i * i > x:
+                return i - 1
+            
+            i += 1
